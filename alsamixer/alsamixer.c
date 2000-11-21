86b50130e99854bf4a9d9eeb0449a0c0f3f4f748
Abramo Bagnara <abramo@alsa-project.org>
2000-11-21 17:15:06 +0000

    Changed default ctl name to open

--- alsamixer/alsamixer.c
+++ alsamixer/alsamixer.c
@@ -178,7 +178,7 @@ static int	 mixer_ofs_x = 0;
 static float	 mixer_extra_space = 0;
 static int	 mixer_cbar_height = 0;
 
-static char*	 card_id = "0";
+static char*	 card_id = "hw:0";
 static snd_mixer_t *mixer_handle;
 static char	 mixer_card_name[128];
 static char	 mixer_device_name[128];
