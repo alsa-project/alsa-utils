alsa-project/alsa-utils

commit fce16d9279b6457435b30092c3c2f799ef1f77d4

axfer: add an implementation of waiter for select(2)
This commit adds support of waiter for select(2) system call.

Below lines are examples to use this option:
$ axfer transfer --waiter-type=select -M -P -d 2 -D hw:0,3 /dev/urandom -f dat -vvv
$ axfer transfer --waiter-type=select -M -C -d 2 -D hw:1,0 /dev/null -r 48000 -vvv

Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>
Signed-off-by: Takashi Iwai <tiwai@suse.de>

axfer/waiter-select.c
@@ -1,0 +1,100 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// waiter-select.c - Waiter for event notification by select(2).
+//
+// Copyright (c) 2018 Takashi Sakamoto <o-takashi@sakamocchi.jp>
+//
+// Licensed under the terms of the GNU General Public License, version 2.
+
+#include "waiter.h"
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/select.h>
+
+// Except for POLLERR.
+#define POLLIN_SET	(POLLRDNORM | POLLRDBAND | POLLIN | POLLHUP)
+#define POLLOUT_SET	(POLLWRBAND | POLLWRNORM | POLLOUT)
+#define POLLEX_SET	(POLLPRI)
+
+struct select_state {
+	fd_set rfds_rd;
+	fd_set rfds_wr;
+	fd_set rfds_ex;
+};
+
+static int select_prepare(struct waiter_context *waiter)
+{
+	return 0;
+}
+
+static int select_wait_event(struct waiter_context *waiter, int timeout_msec)
+{
+	struct select_state *state = waiter->private_data;
+	struct pollfd *pfd;
+	int fd_max;
+	struct timeval tv, *tv_ptr;
+	int i;
+	int err;
+
+	FD_ZERO(&state->rfds_rd);
+	FD_ZERO(&state->rfds_wr);
+	FD_ZERO(&state->rfds_ex);
+
+	fd_max = 0;
+	for (i = 0; i < waiter->pfd_count; ++i) {
+		pfd = &waiter->pfds[i];
+
+		if (pfd->events & POLLIN_SET)
+			FD_SET(pfd->fd, &state->rfds_rd);
+		if (pfd->events & POLLOUT_SET)
+			FD_SET(pfd->fd, &state->rfds_wr);
+		if (pfd->events & POLLEX_SET)
+			FD_SET(pfd->fd, &state->rfds_ex);
+		if (pfd->fd > fd_max)
+			fd_max = pfd->fd;
+	}
+
+	if (timeout_msec < 0) {
+		tv_ptr = NULL;
+	} else {
+		tv.tv_sec = 0;
+		tv.tv_usec = timeout_msec * 1000;
+		tv_ptr = &tv;
+	}
+
+	err = select(fd_max + 1, &state->rfds_rd, &state->rfds_wr,
+		     &state->rfds_ex, tv_ptr);
+	if (err < 0)
+		return err;
+
+	for (i = 0; i < waiter->pfd_count; ++i) {
+		pfd = &waiter->pfds[i];
+
+		pfd->revents = 0;
+		if (FD_ISSET(pfd->fd, &state->rfds_rd))
+			pfd->revents |= POLLIN;
+		if (FD_ISSET(pfd->fd, &state->rfds_wr))
+			pfd->revents |= POLLOUT;
+		if (FD_ISSET(pfd->fd, &state->rfds_ex))
+			pfd->revents |= POLLHUP;
+	}
+
+	return 0;
+}
+
+static void select_release(struct waiter_context *waiter)
+{
+	return;
+}
+
+const struct waiter_data waiter_select = {
+	.ops = {
+		.prepare	= select_prepare,
+		.wait_event	= select_wait_event,
+		.release	= select_release,
+	},
+	.private_size = sizeof(struct select_state),
+};

axfer/Makefile.am
@@ -51,4 +51,5 @@ axfer_SOURCES = \
 	xfer-libasound-irq-mmap.c \
 	waiter.h \
 	waiter.c \
-	waiter-poll.c
+	waiter-poll.c \
+	waiter-select.c

axfer/waiter.c
@@ -17,6 +17,7 @@
 static const char *const waiter_type_labels[] = {
 	[WAITER_TYPE_DEFAULT] = "default",
 	[WAITER_TYPE_POLL] = "poll",
+	[WAITER_TYPE_SELECT] = "select",
 };
 
 enum waiter_type waiter_type_from_label(const char *label)
@@ -44,6 +45,7 @@ int waiter_context_init(struct waiter_context *waiter,
 		const struct waiter_data *waiter;
 	} entries[] = {
 		{WAITER_TYPE_POLL,	&waiter_poll},
+		{WAITER_TYPE_SELECT,	&waiter_select},
 	};
 	int i;
 

axfer/waiter.h
@@ -14,6 +14,7 @@
 enum waiter_type {
 	WAITER_TYPE_DEFAULT = 0,
 	WAITER_TYPE_POLL,
+	WAITER_TYPE_SELECT,
 	WAITER_TYPE_COUNT,
 };
 
@@ -53,5 +54,6 @@ struct waiter_data {
 };
 
 extern const struct waiter_data waiter_poll;
+extern const struct waiter_data waiter_select;
 
 #endif

commit 8d00bd3d67f64612e3977e05ada2e77a9ac3893e

axfer: allow to be compiled with glibc-2.11 or former
The program, axfer, was developed in userspace with glibc-2.28. This
userspace is mostly compliant to POSIX:2008 and some additional macros
for poll event are officially available. The glibc supports them as a
default since its v2.12 release. It will be failed to be compiled with
old glibc or the other libraries for C standard APIs.

One of the purpose of axfer is an better alternative of aplay. In a
point of the purpose, it's preferable to be compiled with the old
libraries.

This commit adds conditional macros to be compiled with libraries for
old compliance level of POSIX.

Reported-by: Jay Foster <jay@systech.com>
Fixes: fce16d9279b6 ('axfer: add an implementation of waiter for select(2)')
Signed-off-by: Takashi Sakamoto <o-takashi@sakamocchi.jp>

axfer/waiter-select.c
@@ -15,9 +15,18 @@
 #include <sys/select.h>
 
 // Except for POLLERR.
-#define POLLIN_SET	(POLLRDNORM | POLLRDBAND | POLLIN | POLLHUP)
-#define POLLOUT_SET	(POLLWRBAND | POLLWRNORM | POLLOUT)
+#ifdef POLLRDNORM
+// This program is for userspace compliant to POSIX 2008 (IEEE 1003.1:2008).
+// This is the default compliance level since glibc-2.12 or later.
+# define POLLIN_SET	(POLLRDNORM | POLLRDBAND | POLLIN | POLLHUP)
+# define POLLOUT_SET	(POLLWRBAND | POLLWRNORM | POLLOUT)
+#else
+// However it's allowed to be for old compliance levels.
+# define POLLIN_SET	(POLLIN | POLLHUP)
+# define POLLOUT_SET	(POLLOUT)
+#endif
 #define POLLEX_SET	(POLLPRI)
+
 
 struct select_state {
 	fd_set rfds_rd;