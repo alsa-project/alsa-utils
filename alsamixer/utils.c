4aadfe334f2322b98e26bf1b88957b4678392c27
Jaroslav Kysela <perex@perex.cz>
2020-06-05 08:26:30 +0200

    alsamixer: suppress _XOPEN_SOURCE compile warning
    Signed-off-by: Jaroslav Kysela <perex@perex.cz>

--- alsamixer/utils.c
+++ alsamixer/utils.c
@@ -16,7 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#ifndef _XOPEN_SOURCE
 #define _XOPEN_SOURCE
+#endif
 #include "aconfig.h"
 #include <limits.h>
 #include <stdlib.h>
