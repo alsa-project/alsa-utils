alsa-project/alsa-utils

commit f138117a457944ca73776bd752693d64ec207e55

alsactl: load_state() - initialize config variable for the open_failed case
Fixes: https://github.com/alsa-project/alsa-utils/issues/93
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

alsactl/state.c
@@ -1642,6 +1642,7 @@ int load_state(const char *cfgdir, const char *file,
 	snd_config_t *config;
 	const char *cardname1;
 
+	config = NULL;
 	err = load_configuration(file, &config, &open_failed);
 	if (err < 0 && !open_failed)
 		return err;

commit 6018c2014ac24877b2aa58c2c57f2fd901e6c7b1

alsactl: load_state: Only delete config if one was loaded
If load_configuration fails with open_failed == true, load_state will
jump to the out label without config being initialized and pass this
uninitialized config value to snd_config_delete. This commit fixes this
issue by initializing config with NULL and checking if it is non-null
before invoking snd_config_delete.

Fixes: https://github.com/alsa-project/alsa-utils/pull/94
Fixes: f138117 ("alsactl: load_state() - initialize config variable for the open_failed case")
BugLink: https://github.com/alsa-project/alsa-utils/issues/93
Signed-off-by: Sören Tempel <soeren+git@soeren-tempel.net>
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

alsactl/state.c
@@ -1692,7 +1692,8 @@ int load_state(const char *cfgdir, const char *file,
 	}
 	err = finalerr ? finalerr : snd_card_iterator_error(&iter);
 out:
-	snd_config_delete(config);
+	if (config)
+		snd_config_delete(config);
 	snd_config_update_free_global();
 	return err;
 }