alsa-project/alsa-utils

commit a9b35252b4b82f05bf80a39e212b61a51e6fef30

topology: don't fail when Define section is missing in the included file
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

topology/pre-processor.c
@@ -409,7 +409,7 @@ static int pre_process_include_conf(struct tplg_pre_processor *tplg_pp, snd_conf
 
 		/* forcefully overwrite with defines from the command line */
 		ret = pre_process_add_defines(tplg_pp, *new);
-		if (ret < 0) {
+		if (ret < 0 && ret != -ENOENT) {
 			fprintf(stderr, "Failed to parse arguments in input config\n");
 			goto err;
 		}

commit b5748d329bae8d1d4cf311193f7e9df3396a99a0

topology: use a copy of the command line defines
Fixes: a9b3525 ("topology: don't fail when Define section is missing in the included file")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

topology/pre-processor.c
@@ -249,7 +249,7 @@ static int pre_process_set_defines(struct tplg_pre_processor *tplg_pp, const cha
 
 static int pre_process_add_defines(struct tplg_pre_processor *tplg_pp, snd_config_t *from)
 {
-	snd_config_t *conf_defines;
+	snd_config_t *conf_defines, *conf_tmp;
 	int ret;
 
 	ret = snd_config_search(from, "Define", &conf_defines);
@@ -280,16 +280,6 @@ create:
 			return ret;
 	}
 
-	/*
-	 * merge the command line defines with the variables in the conf file to override
-	 * default values
-	 */
-	ret = snd_config_merge(conf_defines, tplg_pp->define_cfg, true);
-	if (ret < 0) {
-		fprintf(stderr, "Failed to override variable definitions\n");
-		return ret;
-	}
-
 	if (tplg_pp->define_cfg_merged != conf_defines) {
 		/*
 		 * merge back to the main configuration tree (Define subtree)
@@ -299,6 +289,22 @@ create:
 			fprintf(stderr, "Failed to override main variable definitions\n");
 			return ret;
 		}
+	}
+
+	/*
+	 * merge the command line defines with the variables in the conf file to override
+	 * default values; use a copy (merge deletes the source tree)
+	 */
+	ret = snd_config_copy(&conf_tmp, tplg_pp->define_cfg);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to copy variable definitions\n");
+		return ret;
+	}
+	ret = snd_config_merge(tplg_pp->define_cfg_merged, conf_tmp, true);
+	if (ret < 0) {
+		fprintf(stderr, "Failed to override variable definitions\n");
+		snd_config_delete(conf_tmp);
+		return ret;
 	}
 
 	return 0;