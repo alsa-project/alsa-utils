alsa-project/alsa-utils

commit 31820c5f239fca2de12c8d1a7327ccf995188f33

alsamixer: Check the availability of mouse
Let's check the availiabiy via has_mouse().  Otherwise the program
aborts unexpectedly just focusing on my rxvt terminal :-(

Signed-off-by: Takashi Iwai <tiwai@suse.de>

alsamixer/mainloop.c
@@ -50,7 +50,7 @@ void initialize_curses(bool use_color, bool use_mouse)
 #endif
 	window_size_changed(); /* update screen_lines/cols */
 	init_colors(use_color);
-	if (use_mouse)
+	if (use_mouse && has_mouse())
 		mousemask(ALL_MOUSE_EVENTS, NULL);
 	snd_lib_error_set_handler(black_hole_error_handler);
 }

commit c511b49bf697e05df4b0987d0fd81e3329f64ce6

alsamixer: Revert has_mouse() check
has_mouse() function of ncurses doesn't seem working reliably.
Revert the previous change for addressing the regressions.

Fixes: 31820c5f239f ("alsamixer: Check the availability of mouse")
BugLink: https://github.com/alsa-project/alsa-utils/issues/139
Signed-off-by: Takashi Iwai <tiwai@suse.de>

alsamixer/mainloop.c
@@ -50,8 +50,9 @@ void initialize_curses(bool use_color, bool use_mouse)
 #endif
 	window_size_changed(); /* update screen_lines/cols */
 	init_colors(use_color);
-	if (use_mouse && has_mouse())
+	if (use_mouse)
 		mousemask(ALL_MOUSE_EVENTS, NULL);
+
 	snd_lib_error_set_handler(black_hole_error_handler);
 }
 