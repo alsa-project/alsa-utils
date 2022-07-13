alsa-project/alsa-utils

commit 9a8fcec5aaa8c9104f3ad261d1a610a9dbffca2e

amidi: add timestamp option for dump
Added -T / --timestamp option (with parameter "realtime", "monotonic" or "raw") which prints
a timestamp before each received message when using the --dump mode.

Fixes: https://github.com/alsa-project/alsa-utils/pull/108
Signed-off-by: Folkert van Heusden <mail@vanheusden.com>
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

amidi/amidi.c
@@ -36,11 +36,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <alsa/asoundlib.h>
+#include <time.h>
 #include "aconfig.h"
 #include "version.h"
 
 #define NSEC_PER_SEC 1000000000L
 
+static int do_print_timestamp = 0;
 static int do_device_list, do_rawmidi_list;
 static char *port_name = "default";
 static char *send_file_name;
@@ -80,6 +82,10 @@ static void usage(void)
 		"-r, --receive=file              write received data into a file\n"
 		"-S, --send-hex=\"...\"            send hexadecimal bytes\n"
 		"-d, --dump                      print received data as hexadecimal bytes\n"
+		"-T, --timestamp=...             adds a timestamp in front of each dumped message\n"
+		"                realtime\n"
+		"                monotonic\n"
+		"                raw\n"
 		"-t, --timeout=seconds           exits when no data has been received\n"
 		"                                for the specified duration\n"
 		"-a, --active-sensing            include active sensing bytes\n"
@@ -356,7 +362,7 @@ static void parse_data(void)
 /*
  * prints MIDI commands, formatting them nicely
  */
-static void print_byte(unsigned char byte)
+static void print_byte(unsigned char byte, struct timespec *ts)
 {
 	static enum {
 		STATE_UNKNOWN,
@@ -426,7 +432,18 @@ static void print_byte(unsigned char byte)
 		if (running_status)
 			fputs("\n  ", stdout);
 	}
-	printf("%c%02X", newline ? '\n' : ' ', byte);
+
+	if (newline) {
+		printf("\n");
+
+		/* Nanoseconds does not make a lot of sense for serial MIDI (the
+		 * 31250 bps one) but I'm not sure about MIDI over USB.
+		 */
+		if (do_print_timestamp)
+			printf("%lld.%.9ld) ", (long long)ts->tv_sec, ts->tv_nsec);
+	}
+
+	printf("%02X", byte);
 }
 
 static void sig_handler(int dummy)
@@ -454,7 +471,7 @@ static void add_send_hex_data(const char *str)
 
 int main(int argc, char *argv[])
 {
-	static const char short_options[] = "hVlLp:s:r:S::dt:aci:";
+	static const char short_options[] = "hVlLp:s:r:S::dt:aci:T:";
 	static const struct option long_options[] = {
 		{"help", 0, NULL, 'h'},
 		{"version", 0, NULL, 'V'},
@@ -465,6 +482,7 @@ int main(int argc, char *argv[])
 		{"receive", 1, NULL, 'r'},
 		{"send-hex", 2, NULL, 'S'},
 		{"dump", 0, NULL, 'd'},
+		{"timestamp", 1, NULL, 'T'},
 		{"timeout", 1, NULL, 't'},
 		{"active-sensing", 0, NULL, 'a'},
 		{"clock", 0, NULL, 'c'},
@@ -475,6 +493,7 @@ int main(int argc, char *argv[])
 	int ignore_active_sensing = 1;
 	int ignore_clock = 1;
 	int do_send_hex = 0;
+	clockid_t cid = CLOCK_REALTIME;
 	struct itimerspec itimerspec = { .it_interval = { 0, 0 } };
 
 	while ((c = getopt_long(argc, argv, short_options,
@@ -508,6 +527,19 @@ int main(int argc, char *argv[])
 			break;
 		case 'd':
 			dump = 1;
+			break;
+		case 'T':
+			do_print_timestamp = 1;
+			if (optarg == NULL)
+				error("Clock type missing");
+			else if (strcasecmp(optarg, "realtime") == 0)
+				cid = CLOCK_REALTIME;
+			else if (strcasecmp(optarg, "monotonic") == 0)
+				cid = CLOCK_MONOTONIC;
+			else if (strcasecmp(optarg, "raw") == 0)
+				cid = CLOCK_MONOTONIC_RAW;
+			else
+				error("Clock type not known");
 			break;
 		case 't':
 			if (optarg)
@@ -611,6 +643,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (inputp) {
+		int need_timestamp = 0;
 		int read = 0;
 		int npfds;
 		struct pollfd *pfds;
@@ -644,16 +677,23 @@ int main(int argc, char *argv[])
 				goto _exit;
 			}
 		}
+
 		for (;;) {
 			unsigned char buf[256];
 			int i, length;
 			unsigned short revents;
+			struct timespec ts;
 
 			err = poll(pfds, npfds, -1);
 			if (stop || (err < 0 && errno == EINTR))
 				break;
 			if (err < 0) {
 				error("poll failed: %s", strerror(errno));
+				break;
+			}
+
+			if (clock_gettime(cid, &ts) < 0) {
+				error("clock_getres (%d) failed: %s", cid, strerror(errno));
 				break;
 			}
 
@@ -688,14 +728,12 @@ int main(int argc, char *argv[])
 				continue;
 			read += length;
 
-			if (receive_file != -1) {
-				ssize_t wlength = write(receive_file, buf, length);
-				if (wlength != length)
-					error("write error: %s", wlength < 0 ? strerror(errno) : "short");
-			}
+			if (receive_file != -1)
+				write(receive_file, buf, length);
 			if (dump) {
 				for (i = 0; i < length; ++i)
-					print_byte(buf[i]);
+					print_byte(buf[i], &ts);
+
 				fflush(stdout);
 			}
 

commit 752092d0ea649e3b7181209b128ad03d452cba22

amidi: restore space between bytes
Commit 9a8fcec ("amidi: add timestamp option for dump") removed the
space between bytes when dumping MIDI, changing the output from:

	90 45 40
	80 45 00

to:

	904540
	804500

It seems that this was an unintentional side effect of refactoring the
code to add the new timestamp output but the result is less readable
than it was before.

Restore the space between bytes in the same message.

Fixes: 9a8fcec ("amidi: add timestamp option for dump")
Signed-off-by: John Keeping <john@metanate.com>

amidi/amidi.c
@@ -433,14 +433,12 @@ static void print_byte(unsigned char byte, struct timespec *ts)
 			fputs("\n  ", stdout);
 	}
 
-	if (newline) {
-		printf("\n");
-
+	putchar(newline ? '\n' : ' ');
+	if (newline && do_print_timestamp) {
 		/* Nanoseconds does not make a lot of sense for serial MIDI (the
 		 * 31250 bps one) but I'm not sure about MIDI over USB.
 		 */
-		if (do_print_timestamp)
-			printf("%lld.%.9ld) ", (long long)ts->tv_sec, ts->tv_nsec);
+		printf("%lld.%.9ld) ", (long long)ts->tv_sec, ts->tv_nsec);
 	}
 
 	printf("%02X", byte);