alsa-project/alsa-utils

commit 4b959a447aa1e9ce8d0076fc59fa71ff15f025c3

aplay: Fix for arecord recording ghost data
When recording we should only write the amount of data read to output
file instead of copying whole buffer. This fixes glitches appearing at
the end of recorded file, when stopping recording.

Fixes: https://github.com/alsa-project/alsa-utils/pull/150
Signed-off-by: Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

aplay/aplay.c
@@ -2268,7 +2268,7 @@ static ssize_t pcm_read(u_char *data, size_t rcount)
 		}
 	}
 abort:
-	return rcount;
+	return result;
 }
 
 static ssize_t pcm_readv(u_char **data, unsigned int channels, size_t rcount)
@@ -3326,7 +3326,7 @@ static void capture(char *orig_name)
 			}
 			count -= c;
 			rest -= c;
-			fdcount += c;
+			fdcount += save;
 		}
 
 		/* re-enable SIGUSR1 signal */

alsaucm/go.sh
@@ -5,8 +5,8 @@
 #GDB="valgrind --leak-check=yes --show-reachable=yes"
 #GDB="perf stat"
 PROG=./alsaucm
-PROG=/home/perex/git/pipewire/builddir/spa/plugins/alsa/spa-acp-tool
-PROG="$HOME/git/pulseaudio/build/src/daemon/pulseaudio -n -F $HOME/git/pulseaudio/build/src/daemon/default.pa -p $HOME/git/pulseaudio/build/src/modules/"
+#PROG=/home/perex/git/pipewire/builddir/spa/plugins/alsa/spa-acp-tool
+#PROG="$HOME/git/pulseaudio/build/src/daemon/pulseaudio -n -F $HOME/git/pulseaudio/build/src/daemon/default.pa -p $HOME/git/pulseaudio/build/src/modules/"
 #PROG=pulseaudio
 
 #ALSA_CONFIG_UCM="$HOME/alsa/alsa-ucm-conf/ucm" \

commit 6ee086363a62c9692787860c1edf4f618ad1575a

aplay: fix the capture file length regression
The commit 4b959a4 ("aplay: Fix for arecord recording ghost data")
intruduced a regression (file may contain more samples than
expected). Correct this.

Fixes: https://github.com/alsa-project/alsa-utils/issues/163
Fixes: 4b959a4 ("aplay: Fix for arecord recording ghost data")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

aplay/aplay.c
@@ -2268,7 +2268,7 @@ static ssize_t pcm_read(u_char *data, size_t rcount)
 		}
 	}
 abort:
-	return result;
+	return result > rcount ? rcount : result;
 }
 
 static ssize_t pcm_readv(u_char **data, unsigned int channels, size_t rcount)