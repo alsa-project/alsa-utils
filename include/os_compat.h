758a6e61b9cac34d7a02369a12fb5f183d89629e
SASANO Takayoshi <uaa@uaa.org.uk>
2022-12-31 16:04:43 +0900

    add include/os_compat.h, well-used OS specific definition

---
+++ include/os_compat.h
@@ -0,0 +1,50 @@
+/*
+ * ALSA lib - compatibility header for supporting various OSes
+ * Copyright (C) 2022 by Takayoshi SASANO <uaa@cvs.openbsd.org>
+ *
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as
+ *   published by the Free Software Foundation; either version 2.1 of
+ *   the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef __OS_COMPAT_H
+#define __OS_COMPAT_H
+
+/* workaround for snd_timestamp_t -> struct timeval, "alsaloop.h" */
+#include "aconfig.h"
+#if HAVE_SYS_TIME_H
+#include <sys/time.h>
+#endif
+
+#ifndef ESTRPIPE
+#define ESTRPIPE EPIPE
+#endif
+
+#if defined(__NetBSD__) || defined(__DragonFly__) || defined(__OpenBSD__) || defined(__FreeBSD__)
+#define lseek64 lseek
+#define off64_t off_t
+#endif
+
+#ifndef SCHED_IDLE
+#define SCHED_IDLE SCHED_OTHER
+#endif
+
+#if defined(__OpenBSD__)
+/* these functions in <sched.h> are not implemented */
+#define sched_getparam(pid, param) (-1)
+#define sched_setscheduler(pid, policy, param) (-1)
+#endif
+
+#endif
