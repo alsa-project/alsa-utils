alsa-project/alsa-utils

commit 181e19017dde9f7e4e58654685de3bca3146369c

aplaymidi: fix the verbose compilation warnings for latest gcc
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

seq/aplaymidi/arecordmidi.c
@@ -455,7 +455,7 @@ static void record_port_numbers(void)
 
 static void record_event(const snd_seq_event_t *ev)
 {
-	unsigned int i;
+	int i;
 	struct smf_track *track;
 
 	/* ignore events without proper timestamps */
@@ -583,7 +583,7 @@ static void record_event(const snd_seq_event_t *ev)
 		else
 			command(track, 0xf7), i = 0;
 		var_value(track, ev->data.ext.len - i);
-		for (; i < ev->data.ext.len; ++i)
+		for (; i < (int)ev->data.ext.len; ++i)
 			add_byte(track, ((unsigned char*)ev->data.ext.ptr)[i]);
 		break;
 	default:
@@ -719,7 +719,7 @@ static void version(void)
 	fputs("arecordmidi version " SND_UTIL_VERSION_STR "\n", stderr);
 }
 
-static void sighandler(int sig)
+static void sighandler(int)
 {
 	stop = 1;
 }

seq/aplaymidi/aplaymidi.c
@@ -624,7 +624,7 @@ static void handle_big_sysex(snd_seq_event_t *ev)
 	if (length > MIDI_BYTES_PER_SEC)
 		ev->data.ext.len = MIDI_BYTES_PER_SEC;
 	event_size = snd_seq_event_length(ev);
-	if (event_size + 1 > snd_seq_get_output_buffer_size(seq)) {
+	if (event_size + 1 > (ssize_t)snd_seq_get_output_buffer_size(seq)) {
 		err = snd_seq_drain_output(seq);
 		check_snd("drain output", err);
 		err = snd_seq_set_output_buffer_size(seq, event_size + 1);
@@ -803,7 +803,7 @@ static void play_midi(void)
 		for (i = 0; i < num_tracks; ++i) {
 			struct track *track = &tracks[i];
 			struct event *e2 = track->current_event;
-			if (e2 && e2->tick < min_tick) {
+			if (e2 && e2->tick < (unsigned int)min_tick) {
 				min_tick = e2->tick;
 				event = e2;
 				event_track = track;
@@ -988,7 +988,7 @@ int main(int argc, char *argv[])
 		{"delay", 1, NULL, 'd'},
 		{0}
 	};
-	int c, err;
+	int c;
 	int do_list = 0;
 
 	init_seq();
@@ -1029,6 +1029,7 @@ int main(int argc, char *argv[])
 
 #ifdef HAVE_SEQ_CLIENT_INFO_GET_MIDI_VERSION
 	if (ump_mode) {
+		int err;
 		err = snd_seq_set_client_midi_version(seq, SND_SEQ_CLIENT_UMP_MIDI_1_0);
 		check_snd("set midi version", err);
 	}

commit 66112d60e4ce68720022eaf26080c0f1a2caf81f

seq: use ATTRIBUTE_UNUSED instead remove argument name
We need to support older compilers than GCC 11.

Link: https://github.com/alsa-project/alsa-utils/issues/233
Fixes: 181e190 ("aplaymidi: fix the verbose compilation warnings for latest gcc")
Fixes: a03377a ("aseqnet: fix the verbose compilation warnings for latest gcc")
Fixes: 429c32a ("aseqdump: fix the verbose compilation warnings for latest gcc")
Fixes: 0b48dd6 ("aconnect: fix the verbose compilation warnings for latest gcc")
Signed-off-by: Jaroslav Kysela <perex@perex.cz>

seq/aconnect/aconnect.c
@@ -193,7 +193,8 @@ static void do_search_port(snd_seq_t *seq, int perm, action_func_t do_action)
 }
 
 
-static void print_port(snd_seq_t *, snd_seq_client_info_t *cinfo,
+static void print_port(snd_seq_t *seq ATTRIBUTE_UNUSED,
+		       snd_seq_client_info_t *cinfo,
 		       snd_seq_port_info_t *pinfo, int count)
 {
 	if (! count) {
@@ -247,8 +248,10 @@ static void print_port_and_subs(snd_seq_t *seq, snd_seq_client_info_t *cinfo,
 /*
  * remove all (exported) connections
  */
-static void remove_connection(snd_seq_t *seq, snd_seq_client_info_t *,
-			      snd_seq_port_info_t *pinfo, int)
+static void remove_connection(snd_seq_t *seq,
+			      snd_seq_client_info_t *info ATTRIBUTE_UNUSED,
+			      snd_seq_port_info_t *pinfo,
+			      int count ATTRIBUTE_UNUSED)
 {
 	snd_seq_query_subscribe_t *query;
 	snd_seq_port_info_t *port;

seq/aplaymidi/arecordmidi.c
@@ -719,7 +719,7 @@ static void version(void)
 	fputs("arecordmidi version " SND_UTIL_VERSION_STR "\n", stderr);
 }
 
-static void sighandler(int)
+static void sighandler(int sig ATTRIBUTE_UNUSED)
 {
 	stop = 1;
 }

seq/aseqdump/aseqdump.c
@@ -676,7 +676,7 @@ static void version(void)
 	puts("aseqdump version " SND_UTIL_VERSION_STR);
 }
 
-static void sighandler(int)
+static void sighandler(int sig ATTRIBUTE_UNUSED)
 {
 	stop = 1;
 }

seq/aseqnet/aseqnet.c
@@ -334,7 +334,7 @@ static void get_net_addr(struct addrinfo *rp, char *buf, size_t buflen)
 /*
  * signal handler
  */
-static void sigterm_exit(int)
+static void sigterm_exit(int sig ATTRIBUTE_UNUSED)
 {
 	close_files();
 	exit(1);